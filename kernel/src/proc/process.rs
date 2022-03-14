e53e46948f9845e38bad20512d659a45a9a076cd
transistor <trans@jabberwocky.ca>
2022-03-13 22:25:31 -0700

    Improved the process manager a bit
    I had two mutexes for two different values, but I put them into a
    single struct with a mutex around that, so that everything inside
    ProcessManager is mutable

--- kernel/src/proc/process.rs
+++ kernel/src/proc/process.rs
@@ -20,13 +20,15 @@ pub struct Process {
     space: VirtualAddressSpace,
 }
 
+struct ProcessManager {
+    processes: Vec<Process>,
+    current: usize,
+}
+
 unsafe impl Send for Process {}
 unsafe impl Sync for Process {}
 
-pub static PROCESS_LIST: Mutex<Vec<Process>> = Mutex::new(Vec::new());
-//pub static mut PROCESS_LIST: &mut [Option<Process>] = &mut [];
-
-pub static CURRENT_PROC: Mutex<usize> = Mutex::new(0);
+static PROCESS_MANAGER: Mutex<ProcessManager> = Mutex::new(ProcessManager::new());
 
 // TODO need to move this
 #[no_mangle]
@@ -37,49 +39,54 @@ pub fn init_processes() {
 
 }
 
-pub fn create_process() -> *mut u8 {
-    // TODO this is wrong temporarily
-    let pid = 1;
-
-    let mut processes = PROCESS_LIST.lock();
-
-    processes.push(Process {
-        pid,
-        context: Default::default(),
-        space: VirtualAddressSpace::new_user_space(),
-    });
-
-    let index = processes.len() - 1;
-    let proc = &mut processes[index];
-    let entry = proc.space.alloc_mapped(0x77777000, 4096);
-    Context::init(&mut proc.context, (0x77777000 + 4096) as *mut u8, 0x77777000 as *mut u8, proc.space.get_ttbr());
-
-    *CURRENT_PROC.lock() = index;
-    unsafe {
-        // TODO this is temporary to bootstrap the context switching
-        CURRENT_CONTEXT = &mut proc.context as *mut Context;
+impl ProcessManager {
+    const fn new() -> Self {
+        Self {
+            processes: Vec::new(),
+            current: 0,
+        }
     }
 
-    entry
-}
+    fn create_process(&mut self) -> *mut u8 {
+        // TODO this is wrong temporarily
+        let pid = 1;
 
+        self.processes.push(Process {
+            pid,
+            context: Default::default(),
+            space: VirtualAddressSpace::new_user_space(),
+        });
+
+        self.current = self.processes.len() - 1;
+        let proc = &mut self.processes[self.current];
+        let entry = proc.space.alloc_mapped(0x77777000, 4096);
+        Context::init(&mut proc.context, (0x77777000 + 4096) as *mut u8, 0x77777000 as *mut u8, proc.space.get_ttbr());
+
+        unsafe {
+            // TODO this is temporary to bootstrap the context switching
+            CURRENT_CONTEXT = &mut proc.context as *mut Context;
+        }
+
+        entry
+    }
+
+    fn schedule(&mut self) {
+        self.current += 1;
+        if self.current >= self.processes.len() {
+            self.current = 0;
+        }
+
+        let proc = &mut self.processes[self.current];
+        unsafe {
+            // TODO this is temporary to bootstrap the context switching
+            CURRENT_CONTEXT = &mut proc.context as *mut Context;
+        }
+    }
+}
 
 pub fn schedule() {
-    let mut processes = PROCESS_LIST.lock();
-    let mut current = CURRENT_PROC.lock();
-
-    *current += 1;
-    if *current >= processes.len() {
-        *current = 0;
-    }
-
-    let proc = &mut processes[*current];
-    unsafe {
-        // TODO this is temporary to bootstrap the context switching
-        CURRENT_CONTEXT = &mut proc.context as *mut Context;
-    }
+    PROCESS_MANAGER.lock().schedule();
 }
-
 
 const TEST_PROC1: &[u32] = &[0xd4000021, 0xd503205f, 0x17ffffff];
 const TEST_PROC2: &[u32] = &[0xd10043ff, 0xf90003e0, 0xf90007e1, 0x14000001, 0xd4000021, 0x17ffffff];
@@ -96,10 +103,10 @@ pub fn create_test_process() {
         //let ptr = kmalloc(size);
         //printkln!("Alloc: {:x}", ptr as usize);
 
-        let ptr = create_process();
+        let ptr = PROCESS_MANAGER.lock().create_process();
         load_code(ptr.cast(), TEST_PROC1);
 
-        let ptr = create_process();
+        let ptr = PROCESS_MANAGER.lock().create_process();
         load_code(ptr.cast(), TEST_PROC2);
 
         //let code: *mut u32 = ptr.cast();
