5556da7fdbe3bb6a4e781d7a080716bd8fa37f0c
transistor <trans@jabberwocky.ca>
2022-03-19 20:40:41 -0700

    Modified so only the kernel memory accesses aren't cached
    I modified the value of MAIR_EL1 to include a cached memory type
    with index 0, so by default the memory pointed to by the translation
    tables is cached, and then I modified the kernel TTs to use the
    uncached memory type (MAIR index 1, in the TT descriptor bits 4:2)
    for the 1GB blocks.  This means all kernel memory usage should be
    uncache and all user memory should be cached.  Again this is just
    easier than making a static level 2 table for the kernel with the
    entire table filled with 2MB blocks where only the 0x3F00_0000 space
    uses uncached memory

--- kernel/src/arch/aarch64/mmu.s
+++ kernel/src/arch/aarch64/mmu.s
@@ -7,13 +7,15 @@
 
 // Translation Table Level 1
 _kernel_translation_table_l1:
-.quad 0x00000401
-.quad 0x40000401
+.quad 0x00000405
+.quad 0x40000405
+.quad 0x80000405
+.quad 0xc0000405
 .balign 4096
 
 // Translation Table Level 0
 .global _kernel_translation_table_l0
 _kernel_translation_table_l0:
-.quad _kernel_translation_table_l1 - 0xffff000000000000 + 3
+.quad (_kernel_translation_table_l1 - 0xffff000000000000) + 3
 .balign 4096
 
