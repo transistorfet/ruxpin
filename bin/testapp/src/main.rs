1199046201ec89c70e68c55844ed68523effad77
transistor <trans@jabberwocky.ca>
2022-03-20 21:30:49 -0700

    Added a simple test application
    I wrote this a week or two ago and used it to generate the op codes
    in TEST_PROC2, which I manually copied into the kernel.  I might
    change the name of the kernel api library, and I might also remove
    the linker script and use the default settings, but I'll leave that
    for a while in the future

---
+++ bin/testapp/src/main.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start(_argc: isize, _argv: *const *const u8) -> isize {
+    loop {
+        unsafe {
+            asm!(
+                "svc   #7"
+            );
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
