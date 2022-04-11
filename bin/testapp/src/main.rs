19d2f5245eb7cc5407e741e5797c99cc1898dda8
transistor <trans@jabberwocky.ca>
2022-04-11 12:02:26 -0700

    Added separate library for common app code
    It contains the _start symbol and the initialization, as well as
    the panic handler, so that the main() defined in each application
    can be simpler.  It's required that each such application declare
    the ruxpin_app as external at the start of the file in order to
    properly import the panic handler, but that's less boilerplate
    than the app initialization itself

--- bin/testapp/src/main.rs
+++ bin/testapp/src/main.rs
@@ -1,20 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+extern crate ruxpin_app;
 
 use ruxpin_api::api::write;
 use ruxpin_api::types::FileDesc;
 
 #[no_mangle]
-pub extern "C" fn _start(_argc: isize, _argv: *const *const u8) -> isize {
+pub fn main() {
     loop {
         write(FileDesc(0), b"a really cool message that I'd like to see").unwrap();
     }
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
 }
 
